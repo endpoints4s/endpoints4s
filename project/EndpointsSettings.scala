f84b106084d94c097be5cc6e13835373d64944cb
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2018-09-09 13:04:06 +0200

    try using SNAP10, but it might be buggy

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -75,7 +75,7 @@ object EndpointsSettings {
   val circeVersion = "0.9.3"
   val playVersion = "2.6.15"
 
-  val scalaTestVersion = "3.0.1"
+  val scalaTestVersion = "3.2.0-SNAP10"
   val scalaTestDependency = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
   val addScalaTestCrossDependency = libraryDependencies += "org.scalatest" %%% "scalatest" % scalaTestVersion % Test
 
