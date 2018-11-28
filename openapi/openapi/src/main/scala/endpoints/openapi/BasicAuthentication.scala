dc27f0de74ef343bbd7b8bfca3f5dde415d226fb
Piotr Krzemiński <pio.krzeminski@gmail.com>
2018-11-28 14:48:23 +0100

    initial support for OAS3 security schemes

--- openapi/openapi/src/main/scala/endpoints/openapi/BasicAuthentication.scala
+++ openapi/openapi/src/main/scala/endpoints/openapi/BasicAuthentication.scala
@@ -3,6 +3,7 @@ package openapi
 
 import endpoints.algebra.BasicAuthentication.Credentials
 import endpoints.algebra.Documentation
+import endpoints.openapi.model.{OperationSecurity, SecurityScheme}
 
 /**
   * Interpreter for [[algebra.BasicAuthentication]] that produces
@@ -14,9 +15,31 @@ trait BasicAuthentication
   extends algebra.BasicAuthentication
     with Endpoints {
 
-  private[endpoints] lazy val basicAuthenticationHeader: RequestHeaders[Credentials] = header("Authorization")
+  private[endpoints] def basicAuthenticationHeader: RequestHeaders[Credentials] =
+    DocumentedHeaders(Nil) // supported by OAS3 security schemes
 
   private[endpoints] def authenticated[A](response: Response[A], docs: Documentation): Response[Option[A]] =
     DocumentedResponse(401, docs.getOrElse(""), content = Map.empty) :: response
+
+  /**
+    * Describes an endpoint protected by Basic HTTP authentication
+    */
+  override def authenticatedEndpoint[U, E, R, H, UE, HCred, Out](
+    method: Method,
+    url: Url[U],
+    response: Response[R],
+    requestEntity: RequestEntity[E] = emptyRequest,
+    requestHeaders: RequestHeaders[H] = emptyHeaders,
+    unauthenticatedDocs: Documentation = None,
+    summary: Documentation = None,
+    description: Documentation = None,
+    tags: List[String] = Nil
+  )(implicit
+    tuplerUE: Tupler.Aux[U, E, UE],
+    tuplerHCred: Tupler.Aux[H, Credentials, HCred],
+    tuplerUEHCred: Tupler.Aux[UE, HCred, Out]
+  ): Endpoint[Out, Option[R]] =
+    super.authenticatedEndpoint(method, url, response, requestEntity, requestHeaders, unauthenticatedDocs, summary, description, tags)
+      .withSecurity(OperationSecurity("HttpBasic", SecurityScheme.httpBasic))
 
 }
