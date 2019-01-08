8e048c5a0e0c926eaf4145219026fc329e9d4c6d
Scala steward <me@scala-steward.org>
2019-01-08 14:19:38 +0100

    Update play, play-ahc-ws, play-netty-server to 2.6.21

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -69,7 +69,7 @@ object EndpointsSettings {
 
   val circeVersion = "0.11.0"
   val playjsonVersion = "2.6.13"
-  val playVersion = "2.6.20"
+  val playVersion = "2.6.21"
   val sttpVersion = "1.5.2"
 
   val scalaTestVersion = "3.0.5"
