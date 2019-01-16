3939775a07054a8e37b8c18911fb2990e83ef55e
Devon Stewart <dstewart@twilio.com>
2019-01-15 16:16:34 -0800

    Extending xhr thenable tests to represent multiple header parameters

--- xhr/client/src/test/scala/endpoints/xhr/EndpointsTest.scala
+++ xhr/client/src/test/scala/endpoints/xhr/EndpointsTest.scala
@@ -2,10 +2,17 @@ package endpoints.xhr
 
 import org.scalatest.FreeSpec
 
-object Fixtures extends thenable.Endpoints {
+// Separation of the Algebra here is important, due to implementation details
+// of the underlying representation of header in the xhr algebra.
+trait FixturesAlgebra extends endpoints.algebra.Endpoints {
   val foo = endpoint(get(path / "foo" / segment[String]()), emptyResponse())
   val bar = endpoint(post(path / "bar" /? qs[Int]("quux"), emptyRequest), emptyResponse())
+  // Currently, the fact that this line compiles is a test, as there's no way
+  // to inspect the result of constructing headers at the moment.
+  val baz = endpoint(post(path / "baz", emptyRequest, header("quuz") ++ header("corge") ++ optHeader("grault")), emptyResponse())
 }
+
+object Fixtures extends FixturesAlgebra with thenable.Endpoints
 
 // TODO try to use traits defined in algebra tests.
 // It cannot be simply reused because dependency on wiremock which is not available for js
