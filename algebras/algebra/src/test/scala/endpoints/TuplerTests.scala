21c5781e7f9c7789c1220e4cf9d5e88cf237bd78
Thomas Droxler <thomas.droxler@gmail.com>
2019-08-09 07:51:04 +0200

    Fix for - 93d924e Add More Tupler

--- algebras/algebra/src/test/scala/endpoints/TuplerTests.scala
+++ algebras/algebra/src/test/scala/endpoints/TuplerTests.scala
@@ -4,11 +4,15 @@ class TuplerTests {
 
   def tupling[A, B](a: A, b: B)(implicit tupler: Tupler[A, B]): tupler.Out = tupler(a, b)
 
-  def forall[A, B, C, D](a: A, b: B, c: C, d: D): Unit = {
+  def forall[A, B, C, D, E](a: A, b: B, c: C, d: D, e: E): Unit = {
     tupling(a, b): (A, B)
     tupling(a, (b, c)): (A, B, C)
     tupling((a, b), c): (A, B, C)
     tupling((a, b), (c, d)): (A, B, C, D)
+    tupling(a, (b, c, d, e)): (A, B, C, D, E)
+    tupling((a, b), (c, d, e)): (A, B, C, D, E)
+    tupling((a, b, c), d): (A, B, C, D)
+    tupling((a, b, c, d), e): (A, B, C, D, E)
     tupling(a, ()): A
     tupling((), a): A
     tupling((), ()): Unit
