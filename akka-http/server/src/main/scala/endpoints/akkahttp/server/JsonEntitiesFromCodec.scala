TouK/endpoints

Improve error messages reported by servers
Instead of just returning a Bad Request (400) response with no entity in case of client error, we now return a list of error messages. Fixes #307.

It also standardizes the behavior of the server interpreters: first, they tests whether an incoming request matches a given endpoint definition by checking whether its URL has the same number of segments as the endpoint and if the static segments of the endpoint have the same value in the request URL. Second, the request parameters (dynamic URL segments, query string parameters and header values) are decoded. Third, the request entity is decoded. These three steps are run sequentially: the request parameters are decoded only after the servers have checked that the request matches the endpoint definition, and the request entity is decoded only after the servers have successfully decoded the request parameters. But within a step, the validations are run independently of each others: a request that contains both an invalid path segment and an invalid query string parameter will produce two error messages.

As a consequence, authentication handling has to be done at the level of `Request[A]` instead of `RequestHeaders[A]` (the latter doesn’t anymore have the power of returning an HTTP response). In practice, this means that the `BasicAuthentication` trait now defines a custom `authenticatedRequest` constructor instead of a `basicAuthenticationHeader` constructor.

akka-http/server/src/main/scala/endpoints/akkahttp/server/JsonEntitiesFromCodec.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.marshalling.Marshaller
 import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
+import endpoints.{Invalid, Valid, Validated}
 import endpoints.algebra.Codec
 
 /**
@@ -15,11 +16,14 @@ import endpoints.algebra.Codec
 trait JsonEntitiesFromCodec extends Endpoints with endpoints.algebra.JsonEntitiesFromCodec {
 
   def jsonRequest[A](implicit codec: Codec[String, A]): RequestEntity[A] = {
-    implicit val fromEntityUnmarshaller: FromEntityUnmarshaller[A] =
+    implicit val fromEntityUnmarshaller: FromEntityUnmarshaller[Validated[A]] =
       Unmarshaller.stringUnmarshaller
         .forContentTypes(MediaTypes.`application/json`)
-        .map(data => codec.decode(data).fold(throw _, identity))
-    Directives.entity[A](implicitly)
+        .map(data => codec.decode(data))
+    Directives.entity[Validated[A]](implicitly).flatMap {
+      case Valid(a)     => Directives.provide(a)
+      case inv: Invalid => malformedRequest(inv)
+    }
   }
 
   def jsonResponse[A](implicit codec: Codec[String, A]): ResponseEntity[A] =

akka-http/server/src/main/scala/endpoints/akkahttp/server/MuxEndpoints.scala
@@ -32,9 +32,13 @@ trait MuxEndpoints extends algebra.MuxEndpoints with Endpoints {
       encoder: Encoder[Resp, Transport]
     ): Route =
       request { request =>
-        Directives.onComplete(handler(decoder.decode(request).right.get /* TODO Handle failure */ .asInstanceOf[Req {type Response = Resp}])) {
-          case Success(result) => response(encoder.encode(result))
-          case Failure(ex) => Directives.complete(ex)
+        decoder.decode(request) match {
+          case inv: Invalid => malformedRequest(inv)
+          case Valid(req)   =>
+            Directives.onComplete(handler(req.asInstanceOf[Req { type Response = Resp }])) {
+              case Success(result) => response(encoder.encode(result))
+              case Failure(ex)     => malformedRequest(Invalid("Invalid request entity"))
+            }
         }
       }
 

play/server-playjson/src/main/scala/endpoints/play/server/playjson/JsonSchemaEntities.scala
@@ -1,10 +1,11 @@
 package endpoints.play.server.playjson
 
-import endpoints.algebra
+import endpoints.{Invalid, algebra}
 import endpoints.play.server.Endpoints
 import play.api.http.Writeable
-import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.Results
+import play.api.libs.json.{JsPath, JsValue, Json, JsonValidationError}
+
+import scala.util.Try
 
 /**
   * Interpreter for [[algebra.JsonSchemaEntities]] that uses Play JSON [[play.api.libs.json.Reads]] to decode
@@ -16,8 +17,18 @@ trait JsonSchemaEntities extends Endpoints with algebra.JsonSchemaEntities with 
 
   def jsonRequest[A: JsonSchema]: RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { text =>
-      Json.parse(text).validate(implicitly[JsonSchema[A]].reads).asEither
-        .left.map(ignoredError => Results.BadRequest)
+      Try(Json.parse(text)).toEither.left.map(_ => Invalid("Unable to parse entity as JSON"))
+        .right.flatMap { json =>
+          def showErrors(errors: collection.Seq[(JsPath, collection.Seq[JsonValidationError])]): Invalid =
+            Invalid((
+              for {
+                (path, pathErrors) <- errors.iterator
+                error <- pathErrors
+              } yield s"${error.message} for ${path.toJsonString}"
+            ).toSeq)
+          json.validate(implicitly[JsonSchema[A]].reads).asEither.left.map(showErrors)
+        }
+        .left.map(malformedRequest)
     }
 
   def jsonResponse[A: JsonSchema]: ResponseEntity[A] =

play/server/src/main/scala/endpoints/play/server/JsonEntitiesFromCodec.scala
@@ -1,8 +1,8 @@
 package endpoints.play.server
 
+import endpoints.{Invalid, Valid}
 import endpoints.algebra.Codec
 import play.api.http.{ContentTypes, Writeable}
-import play.api.mvc.Results
 
 /**
   * Interpreter for [[endpoints.algebra.JsonEntitiesFromCodec]] that decodes JSON requests
@@ -16,7 +16,10 @@ trait JsonEntitiesFromCodec extends Endpoints with endpoints.algebra.JsonEntitie
 
   def jsonRequest[A](implicit codec: Codec[String, A]): RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { body =>
-      codec.decode(body).left.map(ignoredError => Results.BadRequest)
+      codec.decode(body) match {
+        case Valid(value) => Right(value)
+        case inv: Invalid => Left(malformedRequest(inv))
+      }
     }
 
   def jsonResponse[A](implicit codec: Codec[String, A]): ResponseEntity[A] = {

endpoints4s/endpoints4s

akka-http/server/src/main/scala/endpoints/akkahttp/server/JsonEntitiesFromCodec.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.marshalling.Marshaller
 import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
+import endpoints.{Invalid, Valid, Validated}
 import endpoints.algebra.Codec
 
 /**
@@ -15,11 +16,14 @@ import endpoints.algebra.Codec
 trait JsonEntitiesFromCodec extends Endpoints with endpoints.algebra.JsonEntitiesFromCodec {
 
   def jsonRequest[A](implicit codec: Codec[String, A]): RequestEntity[A] = {
-    implicit val fromEntityUnmarshaller: FromEntityUnmarshaller[A] =
+    implicit val fromEntityUnmarshaller: FromEntityUnmarshaller[Validated[A]] =
       Unmarshaller.stringUnmarshaller
         .forContentTypes(MediaTypes.`application/json`)
-        .map(data => codec.decode(data).fold(throw _, identity))
-    Directives.entity[A](implicitly)
+        .map(data => codec.decode(data))
+    Directives.entity[Validated[A]](implicitly).flatMap {
+      case Valid(a)     => Directives.provide(a)
+      case inv: Invalid => handleClientErrors(inv)
+    }
   }
 
   def jsonResponse[A](implicit codec: Codec[String, A]): ResponseEntity[A] =

akka-http/server/src/main/scala/endpoints/akkahttp/server/MuxEndpoints.scala
@@ -32,9 +32,13 @@ trait MuxEndpoints extends algebra.MuxEndpoints with Endpoints {
       encoder: Encoder[Resp, Transport]
     ): Route =
       request { request =>
-        Directives.onComplete(handler(decoder.decode(request).right.get /* TODO Handle failure */ .asInstanceOf[Req {type Response = Resp}])) {
-          case Success(result) => response(encoder.encode(result))
-          case Failure(ex) => Directives.complete(ex)
+        decoder.decode(request) match {
+          case inv: Invalid => handleClientErrors(inv)
+          case Valid(req)   =>
+            Directives.onComplete(handler(req.asInstanceOf[Req { type Response = Resp }])) {
+              case Success(result) => response(encoder.encode(result))
+              case Failure(ex)     => handleClientErrors(Invalid("Invalid request entity"))
+            }
         }
       }
 

play/server-playjson/src/main/scala/endpoints/play/server/playjson/JsonSchemaEntities.scala
@@ -1,10 +1,11 @@
 package endpoints.play.server.playjson
 
-import endpoints.algebra
+import endpoints.{Invalid, algebra}
 import endpoints.play.server.Endpoints
 import play.api.http.Writeable
-import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.Results
+import play.api.libs.json.{JsPath, JsValue, Json, JsonValidationError}
+
+import scala.util.Try
 
 /**
   * Interpreter for [[algebra.JsonSchemaEntities]] that uses Play JSON [[play.api.libs.json.Reads]] to decode
@@ -16,8 +17,18 @@ trait JsonSchemaEntities extends Endpoints with algebra.JsonSchemaEntities with 
 
   def jsonRequest[A: JsonSchema]: RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { text =>
-      Json.parse(text).validate(implicitly[JsonSchema[A]].reads).asEither
-        .left.map(ignoredError => Results.BadRequest)
+      Try(Json.parse(text)).toEither.left.map(_ => Invalid("Unable to parse entity as JSON"))
+        .right.flatMap { json =>
+          def showErrors(errors: collection.Seq[(JsPath, collection.Seq[JsonValidationError])]): Invalid =
+            Invalid((
+              for {
+                (path, pathErrors) <- errors.iterator
+                error <- pathErrors
+              } yield s"${error.message} for ${path.toJsonString}"
+            ).toSeq)
+          json.validate(implicitly[JsonSchema[A]].reads).asEither.left.map(showErrors)
+        }
+        .left.map(handleClientErrors)
     }
 
   def jsonResponse[A: JsonSchema]: ResponseEntity[A] =

play/server/src/main/scala/endpoints/play/server/JsonEntitiesFromCodec.scala
@@ -1,8 +1,8 @@
 package endpoints.play.server
 
+import endpoints.{Invalid, Valid}
 import endpoints.algebra.Codec
 import play.api.http.{ContentTypes, Writeable}
-import play.api.mvc.Results
 
 /**
   * Interpreter for [[endpoints.algebra.JsonEntitiesFromCodec]] that decodes JSON requests
@@ -16,7 +16,10 @@ trait JsonEntitiesFromCodec extends Endpoints with endpoints.algebra.JsonEntitie
 
   def jsonRequest[A](implicit codec: Codec[String, A]): RequestEntity[A] =
     playComponents.playBodyParsers.tolerantText.validate { body =>
-      codec.decode(body).left.map(ignoredError => Results.BadRequest)
+      codec.decode(body) match {
+        case Valid(value) => Right(value)
+        case inv: Invalid => Left(handleClientErrors(inv))
+      }
     }
 
   def jsonResponse[A](implicit codec: Codec[String, A]): ResponseEntity[A] = {