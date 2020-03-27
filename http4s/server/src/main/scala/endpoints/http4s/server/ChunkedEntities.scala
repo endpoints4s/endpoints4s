d36786cb1280d86f926b176bd00c3c3f924cf1c8
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-03-26 23:42:15 -0400

    Fix build

--- http4s/server/src/main/scala/endpoints/http4s/server/ChunkedEntities.scala
+++ http4s/server/src/main/scala/endpoints/http4s/server/ChunkedEntities.scala
@@ -7,25 +7,27 @@ import org.http4s.Entity
 import org.http4s.headers.`Content-Type`
 import fs2.Chunk
 
-
 trait ChunkedEntities
     extends algebra.ChunkedEntities
     with EndpointsWithCustomErrors {
 
   type Chunks[A] = fs2.Stream[Effect, A]
 
-
   def textChunksRequest: RequestEntity[Chunks[String]] =
     req => Effect.pure(Right(req.bodyAsText))
 
   def textChunksResponse: ResponseEntity[Chunks[String]] =
-    EntityEncoder.encodeBy()(stream => Entity(stream.through(fs2.text.utf8Encode)))
+    EntityEncoder.encodeBy()(stream =>
+      Entity(stream.through(fs2.text.utf8Encode))
+    )
 
   def bytesChunksRequest: RequestEntity[Chunks[Array[Byte]]] =
     req => Effect.pure(Right(req.body.chunks.map(_.toArray)))
 
   def bytesChunksResponse: ResponseEntity[Chunks[Array[Byte]]] =
-    EntityEncoder.encodeBy(`Content-Type`(MediaType.application.`octet-stream`))(stream =>
+    EntityEncoder.encodeBy(
+      `Content-Type`(MediaType.application.`octet-stream`)
+    )(stream =>
       Entity(stream.flatMap(arr => fs2.Stream.chunk(Chunk.array(arr))))
     )
 
@@ -40,15 +42,27 @@ trait ChunkedJsonEntities
       implicit codec: JsonCodec[A]
   ): RequestEntity[Chunks[A]] = {
     val decoder = stringCodec(codec)
-    req => Effect.pure(Right(req.bodyAsText.evalMap(s => decoder.decode(s).fold(Effect.pure, es => Effect.raiseError[A](new Throwable(es.mkString(", ")))))))
+    req =>
+      Effect.pure(
+        Right(
+          req.bodyAsText.evalMap(s =>
+            decoder
+              .decode(s)
+              .fold(
+                Effect.pure,
+                es => Effect.raiseError[A](new Throwable(es.mkString(", ")))
+              )
+          )
+        )
+      )
   }
 
   def jsonChunksResponse[A](
       implicit codec: JsonCodec[A]
   ): ResponseEntity[Chunks[A]] = {
     val encoder = stringCodec(codec)
-    EntityEncoder.encodeBy(`Content-Type`(MediaType.application.`json`))(stream =>
-      Entity(stream.map(encoder.encode).through(fs2.text.utf8Encode))
+    EntityEncoder.encodeBy(`Content-Type`(MediaType.application.`json`))(
+      stream => Entity(stream.map(encoder.encode).through(fs2.text.utf8Encode))
     )
   }
 
