eb2e26cfceeea2d019eb300a665b28412d93f484
bmeesters <bas.meesters@simacan.com>
2020-04-09 15:49:20 +0200

    Fix formatting

--- json-schema/json-schema/src/main/scala/endpoints/algebra/JsonSchemas.scala
+++ json-schema/json-schema/src/main/scala/endpoints/algebra/JsonSchemas.scala
@@ -284,19 +284,19 @@ trait JsonSchemas extends TuplesSchemas with PartialInvariantFunctorSyntax {
   /** The JSON schema of a coproduct that share the same parent type and thus can be widened to that parent type */
   def orElseMergeTagged[A: ClassTag, C >: A, B <: C: ClassTag](
       taggedA: Tagged[A],
-      taggedB: Tagged[B],
-  ): Tagged[C] = choiceTagged(taggedA, taggedB).xmap {
-    case Left(a) => (a: C)
-    case Right(b) => (b: C)
-  } {
-    case b: B => Right(b)
-    case a: A => Left(a)
-    case any =>
-      throw new IllegalStateException(
-        s"Could not match: A = ${implicitly[ClassTag[A]]}, B = ${implicitly[
-          ClassTag[B]]}, C = ${any.getClass}"
-      )
-  }
+      taggedB: Tagged[B]
+  ): Tagged[C] =
+    choiceTagged(taggedA, taggedB).xmap {
+      case Left(a)  => (a: C)
+      case Right(b) => (b: C)
+    } {
+      case b: B => Right(b)
+      case a: A => Left(a)
+      case any =>
+        throw new IllegalStateException(
+          s"Could not match: A = ${implicitly[ClassTag[A]]}, B = ${implicitly[ClassTag[B]]}, C = ${any.getClass}"
+        )
+    }
 
   /** The JSON schema of a record merging the fields of the two given records */
   def zipRecords[A, B](recordA: Record[A], recordB: Record[B])(
