898f6977fe86833acb46784134797a44a1f7f5cb
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-04-21 10:47:35 -0400

    Revert "Remove streaming support for now"
    This reverts commit 307f293f6de91356ddd7212076241b31aa1aefc9.

---
+++ http4s/client/src/main/scala/endpoints/http4s/client/ChunkedEntities.scala
@@ -0,0 +1,64 @@
+package endpoints.http4s.client
+
+import org.http4s.{Request => Http4sRequest, Response => Http4sResponse}
+import fs2.Chunk
+
+trait ChunkedEntities
+    extends endpoints.algebra.ChunkedEntities
+    with EndpointsWithCustomErrors {
+
+  //#stream-type
+  type Chunks[A] = fs2.Stream[Effect, A]
+  //#stream-type
+
+  override def textChunksRequest
+      : (Chunks[String], Http4sRequest[Effect]) => Http4sRequest[Effect] =
+    (stream, req) => req.withEntity(stream)
+
+  override def textChunksResponse
+      : Http4sResponse[Effect] => Effect[Chunks[String]] =
+    res => effect.pure(res.bodyAsText)
+
+  override def bytesChunksRequest
+      : (Chunks[Array[Byte]], Http4sRequest[Effect]) => Http4sRequest[Effect] =
+    (stream, req) =>
+      req.withBodyStream(
+        stream.flatMap(arr => fs2.Stream.chunk(Chunk.array(arr)))
+      )
+
+  override def bytesChunksResponse
+      : Http4sResponse[Effect] => Effect[Chunks[Array[Byte]]] =
+    res => effect.pure(res.body.chunks.map(_.toArray))
+
+}
+
+trait ChunkedJsonEntities
+    extends endpoints.algebra.ChunkedJsonEntities
+    with ChunkedEntities
+    with JsonEntitiesFromCodecs {
+
+  def jsonChunksRequest[A](
+      implicit codec: JsonCodec[A]
+  ): RequestEntity[Chunks[A]] = { (stream, req) =>
+    val encoder = stringCodec(codec)
+    req.withEntity(stream.map(encoder.encode))
+  }
+
+  def jsonChunksResponse[A](
+      implicit codec: JsonCodec[A]
+  ): ResponseEntity[Chunks[A]] = { response =>
+    val decoder = stringCodec[A](codec)
+    val stream =
+      response.bodyAsText.evalMap(s =>
+        decoder
+          .decode(s)
+          .fold(
+            effect.pure,
+            es => effect.raiseError[A](new Throwable(es.mkString(", ")))
+          )
+      )
+    effect.pure(stream)
+
+  }
+
+}
--- http4s/client/src/test/scala/endpoints/http4s/client/Http4sClientEndpointsJsonSchemaTest.scala
+++ http4s/client/src/test/scala/endpoints/http4s/client/Http4sClientEndpointsJsonSchemaTest.scala
@@ -21,16 +21,20 @@ class TestJsonSchemaClient[F[_]: Sync](host: Uri, client: Client[F])
     extends Endpoints[F](host, client)
     with BasicAuthentication
     with JsonEntitiesFromCodecs
+    with ChunkedJsonEntities
     with algebra.BasicAuthenticationTestApi
     with algebra.EndpointsTestApi
     with algebra.JsonFromCodecTestApi
+    with algebra.ChunkedJsonEntitiesTestApi
     with circe.JsonFromCirceCodecTestApi
     with circe.JsonEntitiesFromCodecs
+    with algebra.circe.ChunkedJsonEntitiesTestApi
 
 class Http4sClientEndpointsJsonSchemaTest
     extends client.EndpointsTestSuite[TestJsonSchemaClient[IO]]
     with client.BasicAuthTestSuite[TestJsonSchemaClient[IO]]
-    with client.JsonFromCodecTestSuite[TestJsonSchemaClient[IO]] {
+    with client.JsonFromCodecTestSuite[TestJsonSchemaClient[IO]]
+    with client.ChunkedJsonEntitiesTestSuite[TestJsonSchemaClient[IO]] {
 
   implicit val system = ActorSystem()
   implicit val ctx: ContextShift[IO] = IO.contextShift(global)
@@ -50,6 +54,38 @@ class Http4sClientEndpointsJsonSchemaTest
 
   def encodeUrl[A](url: client.Url[A])(a: A): String =
     url.encodeUrl(a).toOption.get.renderString
+
+  def callStreamedEndpoint[A, B](
+      endpoint: Kleisli[IO, fs2.Stream[IO, A], B],
+      req: Source[A, _]
+  ): Future[B] = {
+    val inputStream = for {
+      q <- fs2.Stream.eval(Queue.unbounded[IO, Option[A]])
+      _ <- fs2.Stream.eval(
+        IO.fromFuture(IO(req.runForeach(a => q.enqueue1(Some(a)))))
+      )
+      _ <- fs2.Stream.eval(q.enqueue1(None))
+      res <- q.dequeue.unNone
+    } yield res
+
+    endpoint.run(inputStream).unsafeToFuture()
+  }
+
+  def callStreamedEndpoint[A, B](
+      endpoint: Kleisli[IO, A, fs2.Stream[IO, B]],
+      req: A
+  ): Future[Seq[Either[String, B]]] =
+    endpoint
+      .run(req)
+      .flatMap(stream =>
+        stream.attempt.map(_.left.map(_.toString)).compile.toList
+      )
+      .unsafeToFuture()
+
+  val streamingClient = new TestJsonSchemaClient[IO](
+    Uri.unsafeFromString(s"http://localhost:$streamingPort"),
+    ahc
+  )
 
   clientTestSuite()
   basicAuthSuite()
