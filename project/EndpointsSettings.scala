43aae0ee1a8bd43bbc456d8ad1f1be66535137c4
Scala Steward <me@scala-steward.org>
2020-05-21 16:27:49 +0200

    Update play, play-ahc-ws, play-netty-server, ... to 2.8.2

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -111,8 +111,8 @@ object EndpointsSettings {
   // --- Common dependencies
 
   val circeVersion = "0.13.0"
-  val playjsonVersion = "2.8.1"
-  val playVersion = "2.8.1"
+  val playjsonVersion = "2.8.2"
+  val playVersion = "2.8.2"
   val sttpVersion = "1.7.2"
   val akkaActorVersion = "2.6.3"
   val akkaHttpVersion = "10.1.12"
