5896a4975c709befb54d25679f72dccc5f3f32df
Roland Reckel <rreckel@vdl.lu>
2020-07-02 15:20:13 +0200

    ContentType "application/json" in jsonRequest in http4s client (#600) (#601)
    * Add ContentType "application/json" to jsonRequest in http4s client interpreter

--- http4s/client/src/main/scala/endpoints4s/http4s/client/JsonEntities.scala
+++ http4s/client/src/main/scala/endpoints4s/http4s/client/JsonEntities.scala
@@ -2,13 +2,18 @@ package endpoints4s.http4s.client
 
 import cats.implicits._
 import endpoints4s.Codec
+import org.http4s.MediaType
+import org.http4s.headers.`Content-Type`
 
 trait JsonEntitiesFromCodecs
     extends EndpointsWithCustomErrors
     with endpoints4s.algebra.JsonEntitiesFromCodecs {
 
   def jsonRequest[A](implicit codec: JsonCodec[A]): RequestEntity[A] =
-    (a, request) => request.withEntity(stringCodec(codec).encode(a))
+    (a, request) =>
+      request
+        .withEntity(stringCodec(codec).encode(a))
+        .withContentType(`Content-Type`(MediaType.application.json))
 
   def jsonResponse[A](implicit codec: JsonCodec[A]): ResponseEntity[A] =
     _.as[String].flatMap(body =>
