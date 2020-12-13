ebb26e105b5788320ec9de7398bfcc9f34b7d92b
Matthieu Leclercq <matthieu@automat.ai>
2020-12-12 22:02:20 -0500

    Add JsonEntitiesFromEncodersAndDecoders in http4s

--- http4s/server/src/main/scala/endpoints4s/http4s/server/JsonEntities.scala
+++ http4s/server/src/main/scala/endpoints4s/http4s/server/JsonEntities.scala
@@ -1,7 +1,7 @@
 package endpoints4s.http4s.server
 
 import cats.implicits._
-import endpoints4s.{Codec, Invalid, Valid, algebra}
+import endpoints4s.{Codec, Decoder, Encoder, Invalid, Valid, algebra}
 import fs2.Chunk
 import org.http4s
 import org.http4s.headers.`Content-Type`
@@ -19,27 +19,10 @@ trait JsonEntitiesFromCodecs extends algebra.JsonEntitiesFromCodecs with Endpoin
    * requests with incorrect specified `Content-Type` still get accepted.
    */
   def jsonRequest[A](implicit codec: JsonCodec[A]): RequestEntity[A] =
-    req =>
-      http4s.EntityDecoder
-        .decodeBy(MediaType.application.json) { (msg: http4s.Media[Effect]) =>
-          http4s.DecodeResult.success(EntityDecoder.decodeText(msg))
-        }
-        .decode(req, strict = true)
-        .leftWiden[Throwable]
-        .rethrowT
-        .flatMap { value =>
-          stringCodec(codec)
-            .decode(value) match {
-            case Valid(a)     => Effect.pure(Right(a))
-            case inv: Invalid => handleClientErrors(inv).map(Left.apply)
-          }
-        }
+    JsonEntities.decodeJsonRequest(this)(stringCodec)
 
   def jsonResponse[A](implicit codec: JsonCodec[A]): ResponseEntity[A] =
-    EntityEncoder[Effect, Chunk[Byte]]
-      .contramap[A](value => Chunk.bytes(stringCodec(codec).encode(value).getBytes()))
-      .withContentType(`Content-Type`(MediaType.application.json))
-
+    JsonEntities.encodeJsonResponse(this)(stringCodec)
 }
 
 /** Interpreter for [[algebra.JsonEntitiesFromSchemas]] that decodes JSON requests
@@ -55,4 +38,50 @@ trait JsonEntitiesFromSchemas
   def stringCodec[A](implicit codec: JsonCodec[A]): Codec[String, A] =
     codec.stringCodec
 
+}
+
+trait JsonEntitiesFromEncodersAndDecoders
+    extends algebra.JsonEntities
+    with EndpointsWithCustomErrors {
+
+  type JsonRequest[A] = Decoder[String, A]
+  type JsonResponse[A] = Encoder[A, String]
+
+  def jsonRequest[A](implicit decoder: Decoder[String, A]): RequestEntity[A] =
+    JsonEntities.decodeJsonRequest(this)(decoder)
+
+  def jsonResponse[A](implicit encoder: Encoder[A, String]): ResponseEntity[A] =
+    JsonEntities.encodeJsonResponse(this)(encoder)
+}
+
+private object JsonEntities {
+
+  def decodeJsonRequest[A](
+      endpoints: EndpointsWithCustomErrors
+  )(decoder: Decoder[String, A]): endpoints.RequestEntity[A] = {
+    import endpoints.Effect
+    req =>
+      http4s.EntityDecoder
+        .decodeBy(MediaType.application.json) { (msg: http4s.Media[Effect]) =>
+          http4s.DecodeResult.success(EntityDecoder.decodeText(msg))
+        }
+        .decode(req, strict = true)
+        .leftWiden[Throwable]
+        .rethrowT
+        .flatMap { value =>
+          decoder.decode(value) match {
+            case Valid(a)     => Effect.pure(Right(a))
+            case inv: Invalid => endpoints.handleClientErrors(inv).map(Left.apply)
+          }
+        }
+  }
+
+  def encodeJsonResponse[A](
+      endpoints: EndpointsWithCustomErrors
+  )(encoder: Encoder[A, String]): endpoints.ResponseEntity[A] = {
+    import endpoints.Effect
+    EntityEncoder[Effect, Chunk[Byte]]
+      .contramap[A](value => Chunk.bytes(encoder.encode(value).getBytes()))
+      .withContentType(`Content-Type`(MediaType.application.json))
+  }
 }
