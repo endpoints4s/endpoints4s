caacb4d3989d163f289e602cece8d0598a643350
Devon Stewart <dstewart@twilio.com>
2021-01-03 13:09:46 -0800

    Tests for #713

--- json-schema/json-schema/src/test/scala/endpoints4s/TuplerTests.scala
+++ json-schema/json-schema/src/test/scala/endpoints4s/TuplerTests.scala
@@ -5,18 +5,45 @@ class TuplerTests {
   def tupling[A, B](a: A, b: B)(implicit tupler: Tupler[A, B]): tupler.Out =
     tupler(a, b)
 
-  def forall[A, B, C, D, E, F](a: A, b: B, c: C, d: D, e: E, f: F): Unit = {
-    tupling(a, b): (A, B)
-    tupling(a, (b, c)): (A, B, C)
-    tupling((a, b), c): (A, B, C)
-    tupling((a, b), (c, d)): (A, B, C, D)
-    tupling(a, (b, c, d, e)): (A, B, C, D, E)
-    tupling((a, b), (c, d, e)): (A, B, C, D, E)
-    tupling((a, b, c), d): (A, B, C, D)
-    tupling((a, b, c, d), e): (A, B, C, D, E)
-    tupling((a, b, c, d, e), f): (A, B, C, D, E, F)
-    tupling(a, ()): A
+  def forall[A, B, C, D, E, F, G, H, I](a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I): Unit = {
     tupling((), a): A
+    tupling(a, ()): A
+    tupling(a, b): (A, B)
+    tupling((a, b), c): (A, B, C)
+    tupling(a, (b, c)): (A, B, C)
+    tupling((a, b, c), d)  : (A, B, C, D)
+    tupling((a, b), (c, d)): (A, B, C, D)
+    tupling(a, (b, c, d))  : (A, (B, C, D))
+    tupling((a, b, c, d), e)  : (A, B, C, D, E)
+    tupling((a, b, c), (d, e)): (A, B, C, (D, E))
+    tupling((a, b), (c, d, e)): (A, B, C, D, E)
+    tupling(a, (b, c, d, e))  : (A, (B, C, D, E))
+    tupling((a, b, c, d, e), f)  : (A, B, C, D, E, F)
+    tupling((a, b, c, d), (e, f)): (A, B, C, D, (E, F))
+    tupling((a, b, c), (d, e, f)): (A, B, C, (D, E, F))
+    tupling((a, b), (c, d, e, f)): (A, B, (C, D, E, F))
+    tupling(a, (b, c, d, e, f))  : (A, (B, C, D, E, F))
+    tupling((a, b, c, d, e, f), g)  : (A, B, C, D, E, F, G)
+    tupling((a, b, c, d, e), (f, g)): (A, B, C, D, E, (F, G))
+    tupling((a, b, c, d), (e, f, g)): (A, B, C, D, (E, F, G))
+    tupling((a, b, c), (d, e, f, g)): (A, B, C, (D, E, F, G))
+    tupling((a, b), (c, d, e, f, g)): (A, B, (C, D, E, F, G))
+    tupling(a, (b, c, d, e, f, g))  : (A, (B, C, D, E, F, G))
+    tupling((a, b, c, d, e, f, g), h)  : (A, B, C, D, E, F, G, H)
+    tupling((a, b, c, d, e, f), (g, h)): (A, B, C, D, E, F, (G, H))
+    tupling((a, b, c, d, e), (f, g, h)): (A, B, C, D, E, (F, G, H))
+    tupling((a, b, c, d), (e, f, g, h)): (A, B, C, D, (E, F, G, H))
+    tupling((a, b, c), (d, e, f, g, h)): (A, B, C, (D, E, F, G, H))
+    tupling((a, b), (c, d, e, f, g, h)): (A, B, (C, D, E, F, G, H))
+    tupling(a, (b, c, d, e, f, g, h))  : (A, (B, C, D, E, F, G, H))
+    tupling(a, (b, c, d, e, f, g, h, i))  : (A, (B, C, D, E, F, G, H, I))
+    tupling((a, b), (c, d, e, f, g, h, i)): (A, B, (C, D, E, F, G, H, I))
+    tupling((a, b, c), (d, e, f, g, h, i)): (A, B, C, (D, E, F, G, H, I))
+    tupling((a, b, c, d), (e, f, g, h, i)): (A, B, C, D, (E, F, G, H, I))
+    tupling((a, b, c, d, e), (f, g, h, i)): (A, B, C, D, E, (F, G, H, I))
+    tupling((a, b, c, d, e, f), (g, h, i)): (A, B, C, D, E, F, (G, H, I))
+    tupling((a, b, c, d, e, f, g), (h, i)): (A, B, C, D, E, F, G, (H, I))
+    tupling((a, b, c, d, e, f, g, h), i)  : (A, B, C, D, E, F, G, H, I)
     tupling((), ()): Unit
   }
 
