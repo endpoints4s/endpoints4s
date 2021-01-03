f79b4b8187848b31e04611eea78e951313262037
Devon Stewart <dstewart@twilio.com>
2021-01-03 13:09:46 -0800

    Tests for #713

--- json-schema/json-schema/src/test/scala/endpoints4s/TuplerTests.scala
+++ json-schema/json-schema/src/test/scala/endpoints4s/TuplerTests.scala
@@ -5,7 +5,7 @@ class TuplerTests {
   def tupling[A, B](a: A, b: B)(implicit tupler: Tupler[A, B]): tupler.Out =
     tupler(a, b)
 
-  def forall[A, B, C, D, E, F](a: A, b: B, c: C, d: D, e: E, f: F): Unit = {
+  def forall[A, B, C, D, E, F](a: A, b: B, c: C, d: D, e: E, f: F, g: G): Unit = {
     tupling(a, b): (A, B)
     tupling(a, (b, c)): (A, B, C)
     tupling((a, b), c): (A, B, C)
@@ -15,6 +15,7 @@ class TuplerTests {
     tupling((a, b, c), d): (A, B, C, D)
     tupling((a, b, c, d), e): (A, B, C, D, E)
     tupling((a, b, c, d, e), f): (A, B, C, D, E, F)
+    tupling((a, b, c), (d, e, f, g)): (A, B, C, D, E, F, G)
     tupling(a, ()): A
     tupling((), a): A
     tupling((), ()): Unit
