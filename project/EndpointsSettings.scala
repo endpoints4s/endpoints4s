9a24e3e483b50b3a6dac6c4d7f15a5a6e7c07bcc
Scala Steward <me@scala-steward.org>
2021-02-04 16:21:21 +0100

    Update ujson to 1.2.3

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -105,7 +105,7 @@ object EndpointsSettings {
   val akkaActorVersion = "2.6.12"
   val akkaHttpVersion = "10.2.3"
   val http4sVersion = "0.21.18"
-  val ujsonVersion = "1.1.0"
+  val ujsonVersion = "1.2.3"
 
   val scalaTestVersion = "3.2.3"
   val scalaTestDependency =
