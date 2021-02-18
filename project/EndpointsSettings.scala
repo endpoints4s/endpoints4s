2c2722a7035556c02becc362d7c31e18fbe8cace
Scala Steward <me@scala-steward.org>
2021-02-18 12:50:46 +0100

    Update scalatest to 3.2.4

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -107,7 +107,7 @@ object EndpointsSettings {
   val http4sVersion = "0.21.18"
   val ujsonVersion = "1.1.0"
 
-  val scalaTestVersion = "3.2.3"
+  val scalaTestVersion = "3.2.4"
   val scalaTestDependency =
     "org.scalatest" %% "scalatest" % scalaTestVersion % Test
   val addScalaTestCrossDependency =
