069c4de7171abbcfe227bd4a1e1fdaf45b251f41
Scala Steward <me@scala-steward.org>
2021-07-29 10:27:43 +0200

    Update http4s-async-http-client, ... to 0.21.25

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -103,7 +103,7 @@ object EndpointsSettings {
   val sttpVersion = "3.3.11"
   val akkaActorVersion = "2.6.15"
   val akkaHttpVersion = "10.2.5"
-  val http4sVersion = "0.21.24"
+  val http4sVersion = "0.21.25"
   val ujsonVersion = "1.4.0"
 
   val scalaTestVersion = "3.2.9"
