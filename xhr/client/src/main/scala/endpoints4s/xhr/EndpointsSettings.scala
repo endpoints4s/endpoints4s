9ae47729ce39e80cf3d613c7970acc80a8afa042
Domantas Petrauskas <dom.petrauskas@gmail.com>
2021-11-25 17:12:08 +0200

    WIP
    Co-authored-by: Julien Richard-Foy <julien@richard-foy.fr>

--- xhr/client/src/main/scala/endpoints4s/xhr/EndpointsSettings.scala
+++ xhr/client/src/main/scala/endpoints4s/xhr/EndpointsSettings.scala
@@ -4,7 +4,7 @@ import endpoints4s.Hashing
 
 import scala.annotation.nowarn
 
-final class EndpointsSettings(val host: Option[String]) extends Serializable {
+final class EndpointsSettings private (val host: Option[String]) extends Serializable {
 
   override def toString =
     s"EndpointsSettings($host)"
