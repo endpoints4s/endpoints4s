7c6aae42e5045e4c7754cb23efbf56bb40052940
Scala Steward <scala_steward@virtuslab.com>
2022-07-05 22:06:42 +0000

    Update scala3-library, ... to 3.1.3

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -66,7 +66,7 @@ object EndpointsSettings {
   )
   val `scala 2.12 to dotty` = Seq(
     scalaVersion := "2.13.8",
-    crossScalaVersions := Seq("2.13.8", "3.1.2", "2.12.13")
+    crossScalaVersions := Seq("2.13.8", "3.1.3", "2.12.13")
   )
 
   val publishSettings = commonSettings ++ Seq(
