86727b7e32948b09dff0a2951148389693f40f48
Scala Steward <scala_steward@virtuslab.com>
2023-01-29 16:15:10 +0000

    Update play-json to 2.9.4

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -105,7 +105,7 @@ object EndpointsSettings {
   // --- Common dependencies
 
   val circeVersion = "0.14.1"
-  val playjsonVersion = "2.9.3"
+  val playjsonVersion = "2.9.4"
   val playVersion = "2.8.13"
   val sttpVersion = "3.3.15"
   val akkaActorVersion = "2.6.17"
