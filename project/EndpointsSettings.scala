e3e62e9e49d44783eea2b78c4c577fd95daa9742
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-08 07:32:53 +0200

    Update scala-library to 2.12.19 (#1242)

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -62,11 +62,11 @@ object EndpointsSettings {
   )
   val `scala 2.12 to 2.13` = Seq(
     scalaVersion := "2.13.12",
-    crossScalaVersions := Seq("2.13.12", "2.12.18")
+    crossScalaVersions := Seq("2.13.12", "2.12.19")
   )
   val `scala 2.12 to dotty` = Seq(
     scalaVersion := "2.13.12",
-    crossScalaVersions := Seq("2.13.12", "3.3.3", "2.12.18")
+    crossScalaVersions := Seq("2.13.12", "3.3.3", "2.12.19")
   )
 
   val publishSettings = commonSettings ++ Seq(
