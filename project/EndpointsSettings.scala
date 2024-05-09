fb644550a8ab5d0b8501a2a49f90c9dae9d5d398
Scala Steward <scala_steward@virtuslab.com>
2024-05-09 14:31:24 +0000

    Update scala-library to 2.13.14

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -57,16 +57,16 @@ object EndpointsSettings {
     libraryDependencies -= "org.scala-lang" % "scala-compiler" % scalaVersion.value % Runtime
   )
   val `scala 2.13` = Seq(
-    scalaVersion := "2.13.12",
-    crossScalaVersions := Seq("2.13.12")
+    scalaVersion := "2.13.14",
+    crossScalaVersions := Seq("2.13.14")
   )
   val `scala 2.12 to 2.13` = Seq(
-    scalaVersion := "2.13.12",
-    crossScalaVersions := Seq("2.13.12", "2.12.19")
+    scalaVersion := "2.13.14",
+    crossScalaVersions := Seq("2.13.14", "2.12.19")
   )
   val `scala 2.12 to dotty` = Seq(
-    scalaVersion := "2.13.12",
-    crossScalaVersions := Seq("2.13.12", "3.3.3", "2.12.19")
+    scalaVersion := "2.13.14",
+    crossScalaVersions := Seq("2.13.14", "3.3.3", "2.12.19")
   )
 
   val publishSettings = commonSettings ++ Seq(
