c04df19cc43148b3cda7d608e3e90184bde0f1ee
Scala Steward <scala_steward@virtuslab.com>
2025-03-17 16:40:35 +0000

    Update circe-core, circe-generic, ... to 0.14.12

--- project/EndpointsSettings.scala
+++ project/EndpointsSettings.scala
@@ -105,7 +105,7 @@ object EndpointsSettings {
 
   // --- Common dependencies
 
-  val circeVersion = "0.14.1"
+  val circeVersion = "0.14.12"
   val playjsonVersion = "2.9.3"
   val playVersion = "2.8.13"
   val sttpVersion = "3.3.18"
